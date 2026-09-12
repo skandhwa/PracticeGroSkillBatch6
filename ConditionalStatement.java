38297f5601252b1dcb7611bb8fb133b16328adc5
skandhwa <113922787+skandhwa@users.noreply.github.com>
2026-09-12 21:56:25 +0530

    commiting my changes

---
+++ ConditionalStatement.java
@@ -0,0 +1,25 @@
+package MyTest1;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=5,c=20;
+		
+		if(a>b)
+		{
+			if(a>c)
+			{
+				System.out.println("a is max");
+			}
+			
+			else
+			{
+				System.out.println("c is max");
+			}
+		}
+		
+
+	}
+
+}
